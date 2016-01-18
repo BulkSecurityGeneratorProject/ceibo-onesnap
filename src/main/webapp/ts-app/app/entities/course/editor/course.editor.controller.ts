cbca58f22372a182368675071bc8171811fc1554
sebastian <sebastian.fondra@gmail.com>
2016-01-18 14:22:55 -0300

    agrego logica para mostrar un listado de Teaches en la vista de edicion de un Course. La funcionalidad no está completa porque no funciona.

--- src/main/webapp/ts-app/app/entities/course/editor/course.editor.controller.ts
+++ src/main/webapp/ts-app/app/entities/course/editor/course.editor.controller.ts
@@ -1,22 +1,39 @@
 /// <reference path="../../../../../typings/tsd.d.ts" />
 /// <reference path="../../../commons/subscriptions.service.ts" />
+/// <reference path="../../teacher/teacher.service.ts" />
 
 module AngularTest.Course {
     'use strict';
 
     export class CourseEditorController {
 
-        static $inject: Array<string> = ['CourseService', 'StreamsService', '$stateParams', 'TeacherService'];
+        static $inject: Array<string> = 
+            ['CourseService', 'StreamsService', '$stateParams' , 'TeacherService'];
     
         public course;
-    
+
         public formFields;
     
         public courseStream;
+
+        private teacherStream;
+
+        private teachersList: Array<any>;
+
+        private loadTeachers () {
+            this.TeacherService.query({ page: 0 , size: 20 });
+            this.teacherStream = this.StreamsService.getStream('/api/teacher');
+            this.teachersList = [];
+            this.teacherStream.subscribe((notification) => {
+                this.teachersList = notification.data;
+            });
+        }
     
         constructor(public CourseService, private StreamsService: StreamsService,
-            $stateParams: ng.ui.IStateParamsService, public TeacherService) {
+            $stateParams: ng.ui.IStateParamsService, private TeacherService) {
 
+            this.loadTeachers();
+            
             if ($stateParams['id']) {
                 CourseService.get({ id: $stateParams['id'] });
                 this.courseStream = 
@@ -36,15 +53,16 @@ module AngularTest.Course {
                     'key': 'name'
                 },
                 {
-                    type: 'select',
-                    key: 'teacher',
-                    templateOptions: {
-                        options: [
-                            {'sapo': 'pepe'},
-                            {'lalo': 'landa'}
-                        ]
+                    'type': 'select',
+                    'key': 'teacher',
+                    'templateOptions': {
+                        'label': 'Teacher',
+                        'displayKey': 'firstName',
+                        'selectProperty': 'id',
+                        'collection': this.teachersList
                     }
                 }
+  
             ];
         }
     
